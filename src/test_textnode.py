66cfe099ef8d4b5b1671b2c386495b197aae463a
blu-Encelado <bluencelado@gmail.com>
2025-10-25 08:47:05 +0200

    clean up

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -19,6 +19,3 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertNotEqual(node, node2)
 
-
-if __name__ == "__main__":
-    unittest.main()
