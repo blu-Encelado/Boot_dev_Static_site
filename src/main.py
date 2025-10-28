268d4277fa29e28466f0839bd6c996b8104d3211
blu-Encelado <bluencelado@gmail.com>
2025-10-28 10:02:54 +0100

    Paths management

--- src/main.py
+++ src/main.py
@@ -1,9 +1,33 @@
 from textnode import *
+import os
+import shutil
 
 print("initialize main.py")
+public = "./public"
+static = "./static"
 
 def main():
-    print(TextNode("This is some anchor text", "link", "https://www.boot.dev"))
+    path_exists = os.path.exists(public)
+    if path_exists:
+        shutil.rmtree(public)
+    
+    os.mkdir(public)
+    path_to_public = os.path.abspath(public)
+
+    
+    static_items_list = os.listdir(static)
+    if len(static_items_list) > 0:
+        for item in static_items_list:
+
+            path_to_item = os.path.abspath(static) + f"/{item}"
+            is_dir = os.path.isdir(path_to_item)
+            if not is_dir:
+                shutil.copy(path_to_item, path_to_public)
+            else:
+                shutil.copytree(path_to_item, path_to_public + f"/{item}")        
+        
+
+
 
 if __name__ == "__main__":
     main()
